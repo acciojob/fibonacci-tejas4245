dbd4eab18c91d2bec2b485f714483e3908ce2a42
tejas4245 <120262626+tejas4245@users.noreply.github.com>
2023-04-11 14:27:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
 // your code here
+	if(num<=1){
+		return num;
+	}
+	let prevFib=0;
+	let currFib=1;
+
+	for(let i=2;i<=num;i++){
+		let nextFib=prevFib+currFib;
+		prevFib=currFib;
+		currFib=nextFib;
+	}
+	retrun currFib;
 }
 
 module.exports = fibonacci;
